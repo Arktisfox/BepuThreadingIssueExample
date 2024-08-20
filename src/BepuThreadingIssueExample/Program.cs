e6fa0b9c2a57fe4e821b1fd2afe6fad14731b4c1
Arktisfox <arktisfox@protonmail.com>
2024-08-20 14:42:48 -0400

    no message

---
+++ src/BepuThreadingIssueExample/Program.cs
@@ -0,0 +1,52 @@
+﻿using BepuPhysics;
+using BepuPhysics.Collidables;
+using BepuUtilities.Memory;
+using System.Numerics;
+
+namespace BepuThreadingIssueExample
+{
+    internal class Program
+    {
+        static BufferPool? BufferPool;
+        static Simulation? Simulation;
+
+        static void Main(string[] args)
+        {
+            BufferPool = new BufferPool();
+            Simulation = Simulation.Create(BufferPool, new NoCollisionCallbacks(), new EmptyPoseIntegratorCallbacks(), new SolveDescription(8, 1));
+
+            
+            BufferPool.Take<Triangle>(2, out var triBuffer);
+
+            ref var triA = ref triBuffer[0];
+            triA.A = new Vector3(-1, 0, -1);
+            triA.B = new Vector3(1, 0, -1);
+            triA.C = new Vector3(1, 0, 1);
+
+            ref var triB = ref triBuffer[0];
+            triA.A = new Vector3(-1, 0, -1);
+            triA.B = new Vector3(1, 0, 11);
+            triA.C = new Vector3(-1, 0, 1);
+
+            var groundplane = new Mesh(triBuffer, Vector3.One, BufferPool);
+            Simulation.Statics.Add(new StaticDescription(RigidPose.Identity, Simulation.Shapes.Add(groundplane)));
+
+            Parallel.For(0, 10000, (i) =>
+            {
+                try
+                {
+                    var sweeper = new Sphere(5.0f);
+                    var sweepHandler = new DemoSweepHandler();
+                    var rayHandler = new DemoRayHandler();
+
+                    Simulation.RayCast(Vector3.UnitY, -Vector3.UnitY, float.MaxValue, ref rayHandler);
+                    Simulation.Sweep(sweeper, RigidPose.Identity, new BodyVelocity(Vector3.Zero), float.MaxValue, BufferPool, ref sweepHandler);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            });
+        }
+    }
+}
---
+++ src/BepuThreadingIssueExample/SweepCastHandlers.cs
@@ -0,0 +1,60 @@
+﻿using BepuPhysics.Collidables;
+using BepuPhysics;
+using System.Numerics;
+using BepuPhysics.Trees;
+using System.Runtime.CompilerServices;
+
+namespace BepuThreadingIssueExample
+{
+    unsafe struct DemoRayHandler : IRayHitHandler
+    {
+        public bool Hit;
+        public Vector3 HitPos;
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool AllowTest(CollidableReference collidable)
+        {
+            return true;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool AllowTest(CollidableReference collidable, int childIndex)
+        {
+            return true;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void OnRayHit(in RayData ray, ref float maximumT, float t, in Vector3 normal, CollidableReference collidable, int childIndex)
+        {
+            Hit = true;
+            HitPos = ray.Origin + (ray.Direction * t);
+        }
+    }
+
+    public unsafe struct DemoSweepHandler : ISweepHitHandler
+    {
+        public int HitCount;
+
+        public bool AllowTest(CollidableReference collidable)
+        {
+            return true;
+        }
+
+        public bool AllowTest(CollidableReference collidable, int child)
+        {
+            return true;
+        }
+
+        public void OnHit(ref float maximumT, float t, in Vector3 hitLocation, in Vector3 hitNormal, CollidableReference collidable)
+        {
+            HitCount++;
+        }
+
+        public void OnHitAtZeroT(ref float maximumT, CollidableReference collidable)
+        {
+
+            HitCount++;
+        }
+    }
+}
